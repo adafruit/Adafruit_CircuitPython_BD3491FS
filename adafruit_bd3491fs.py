e825cd060c965abe27beb76d3805fa01650d876f
siddacious <nospam187+github@gmail.com>
2019-05-26 12:20:09 -0700

    working, possibly releasable version with basic input selection and gain adjustment

--- adafruit_bd3491fs.py
+++ adafruit_bd3491fs.py
@@ -23,7 +23,8 @@
 `adafruit_bd3491fs`
 ================================================================================
 
-A driver for the Rohm BD3491FS audio processor
+CircuitPython library for the Rohm BD3491FS Audio Processor
+
 
 * Author(s): Bryan Siepert
 
@@ -32,12 +33,14 @@ Implementation Notes
 
 **Hardware:**
 
-**Software and Dependencies:**
- * Adafruit CircuitPython firmware for the supported boards:
-    https://github.com/adafruit/circuitpython/releases
- * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
- * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
+* `Adafruit BD3491FS Breakout <https://www.adafruit.com/products>`_"
 
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the supported boards:
+  https://github.com/adafruit/circuitpython/releases
+* Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
+* Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
 """
 
 # imports
@@ -48,8 +51,7 @@ __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_BD3491FS.git"
 from micropython import const
 import adafruit_bus_device.i2c_device as i2cdevice
 from adafruit_register.i2c_struct import UnaryStruct
-from adafruit_register.i2c_bits import RWBits, ROBits
-from adafruit_register.i2c_bit import RWBit
+
 # pylint: disable=bad-whitespace
 _INPUT_SELECTOR	 = const(0x04)
 _INPUT_GAIN      = const(0x06)
@@ -57,17 +59,16 @@ _VOLUME_GAIN_CH1 = const(0x21)
 _VOLUME_GAIN_CH2 = const(0x22)
 _BASS_GAIN       = const(0x51)
 _TREBLE_GAIN     = const(0x57)
-_SURROUND_GAIN        = const(0x78)
-_TEST_MODE       = const(0xF0)
+_SURROUND_GAIN   = const(0x78)
 _SYSTEM_RESET    = const(0xFE)
 # pylint: enable=bad-whitespace
 
-class Input: # pylint: disable=too-few-public-methods
+class Input: # pylint: disable=too-few-public-methods,invalid-name
     """Options for ``active_input``
 
-        +---------------+------------------+
+        +-----------------+------------------+
         | ``Input``       | Input Pair       |
-        +===============+==================+
+        +=================+==================+
         | ``Input.A``     | Inputs A1 and A2 |
         +-----------------+------------------+
         | ``Input.B``     | Inputs B1 and B2 |
@@ -94,157 +95,134 @@ class Input: # pylint: disable=too-few-public-methods
     F = const(0x06)
     SHORT = const(0x05)
     MUTE = const(0x07)
-class InputGain: # pylint: disable=too-few-public-methods
-    """Options for ``active_input``
+class Level: # pylint: disable=too-few-public-methods
+    """Options for ``imput_gain``
 
-        +---------------+------------------+
-        | ``Input``       | Input Pair       |
-        +===============+==================+
-        | ``Input.A``     | Inputs A1 and A2 |
-        +-----------------+------------------+
-        | ``Input.B``     | Inputs B1 and B2 |
-        +-----------------+------------------+
-        | ``Input.C``     | Inputs C1 and C2 |
-        +-----------------+------------------+
-        | ``Input.D``     | Inputs D1 and D2 |
-        +-----------------+------------------+
-        | ``Input.E``     | Inputs E1 and E2 |
-        +-----------------+------------------+
-        | ``Input.F``     | Inputs F1 and F2 |
-        +-----------------+------------------+
-        | ``Input.SHORT`` | Short inputs     |
-        +-----------------+------------------+
-        | ``Input.MUTE``  | Mute all         |
+        +----------------------+-------+
+        | ``Level``            | Value |
+        +======================+=======+
+        | ``Level.LEVEL_0DB``  | 0dB   |
+        +----------------------+-------+
+        | ``Level.LEVEL_2DB``  | 2dB   |
+        +----------------------+-------+
+        | ``Level.LEVEL_4DB``  | 4dB   |
+        +----------------------+-------+
+        | ``Level.LEVEL_6DB``  | 6dB   |
+        +----------------------+-------+
+        | ``Level.LEVEL_8DB``  | 8dB   |
+        +----------------------+-------+
+        | ``Level.LEVEL_10DB`` | 10dB  |
+        +----------------------+-------+
+        | ``Level.LEVEL_12DB`` | 12dB  |
+        +----------------------+-------+
+        | ``Level.LEVEL_14DB`` | 14dB  |
+        +----------------------+-------+
+        | ``Level.LEVEL_16DB`` | 16dB  |
+        +----------------------+-------+
+        | ``Level.LEVEL_20DB`` | 20dB  |
+        +----------------------+-------+
 
     """
-    GAIN_0DB = const(0x00)
-    GAIN_2DB = const(0x01)
-    GAIN_4DB = const(0x02)
-    GAIN_6DB = const(0x03)
-    GAIN_8DB = const(0x04)
-    GAIN_10DB = const(0x05)
-    GAIN_12DB = const(0x06)
-    GAIN_14DB = const(0x07)
-    GAIN_16DB = const(0x08)
-    GAIN_20DB = const(0x0A)
-    GAIN_OFF = const(0x00)
-    GAIN_LOW = const(0x05)
-    GAIN_MED = const(0x0A)
-    GAIN_HIGH = const(0x0F)
+    LEVEL_0DB = const(0x00)
+    LEVEL_2DB = const(0x01)
+    LEVEL_4DB = const(0x02)
+    LEVEL_6DB = const(0x03)
+    LEVEL_8DB = const(0x04)
+    LEVEL_10DB = const(0x05)
+    LEVEL_12DB = const(0x06)
+    LEVEL_14DB = const(0x07)
+    LEVEL_16DB = const(0x08)
+    LEVEL_20DB = const(0x0A)
 
-    
+
 class BD3491FS: # pylint: disable=too-many-instance-attributes
     """Driver for the Rohm BD3491FS audio processor
 
         :param ~busio.I2C i2c_bus: The I2C bus the BD3491FS is connected to.
-        :param address: The I2C device address for the sensor. Default is ``WAKKA WAKKA WAKKA`` but will accept
-            ``WAKKA WAKKA WAKKA`` when the ``SDO`` pin is connected to Ground.
-
     """
 
-    _input_selector = RWBits(3, _INPUT_SELECTOR, 0, 1)
-    _input_gain = RWBits(4, _INPUT_GAIN, 1, 1)
-    _volume_gain_ch1 = RWBits(7, _VOLUME_GAIN_CH1, 0, 1)
-    _volume_gain_ch2 = RWBits(7, _VOLUME_GAIN_CH2, 0, 1)
-    _bass_gain = RWBits(3, _BASS_GAIN, 1, 1)
-    bass_gain_cut = RWBit(_BASS_GAIN, 7)
-    """Bass gain direcrtion. Set to True to cut the bass by the amount set in ``bass_gain``, or false
-        to boost the bass by the given amount"""
-
-    _treble_gain = RWBits(3, _TREBLE_GAIN, 1, 1)
-    _treble_gain_cut = RWBit(_TREBLE_GAIN, 7)
-    _surround_gain = RWBits(4, _SURROUND_GAIN, 0, 1)
-    _surround_mode = RWBit(_SURROUND_GAIN, 7)
-
-    _test_mode = UnaryStruct(_TEST_MODE, "<B")
+    _input_selector = UnaryStruct(_INPUT_SELECTOR, "<B")
+    _input_gain = UnaryStruct(_INPUT_GAIN, "<B")
+    _ch1_attenuation = UnaryStruct(_VOLUME_GAIN_CH1, "<B")
+    _ch2_attenuation = UnaryStruct(_VOLUME_GAIN_CH2, "<B")
     _system_reset = UnaryStruct(_SYSTEM_RESET, "<B")
-    # _reset = RWBit(_CTRL_REG2, 2)
-    # _reset_filter = ROBits(8, _LPFP_RES, 0, 1)
-    # _chip_id = UnaryStruct(_WHO_AM_I, "<B")
 
-    def __init__(self, i2c_bus, address=0x41):
-        self.i2c_device = i2cdevice.I2CDevice(i2c_bus, address)
-        # if self._chip_id != 0xb1:
-        #     raise RuntimeError('Failed to find BD3491FS! Chip ID 0x%x' % self._chip_id)
+    def __init__(self, i2c_bus):
+        self.i2c_device = i2cdevice.I2CDevice(i2c_bus, 0x41)
+        self._current_active_input = 7 # mute
+        self._current_input_gain = 0 # 0dB
+        self._current_ch1_attenuation = 255 # muted
+        self._current_ch2_attenuation = 255 # muted
+        self.reset()
 
-        # self.reset()
-
-
-
-        self._block_updates = True
-        self._interrupt_latch = True
 
     def reset(self):
-        """Reset the sensor, restoring all configuration registers to their defaults"""
-        self._reset = True
-        # wait for the reset to finish
-        while self._reset:
-            pass
+        """Reset the sensor, muting the input, reducting input gain to 0dB, and the output channnel
+        attenuation to maximum"""
+        self._reset = 0x81
 
     @property
     def active_input(self):
-        """The currently selected input"""
-        return self._input_selector
+        """The currently selected input. Must be an ``Input``
+
+        This example sets A1 and A2 to the active input pair.
+        .. code-block:: python
+        bd3491fs.active_input = adafruit_bd3491fs.Input.A
+        """
+        return self._current_active_input
 
     @active_input.setter
     def active_input(self, value):
         self._input_selector = value
+        self._current_active_input = value
 
     @property
     def input_gain(self):
-        """The gain applied to all inputs equally"""
-        return self._input_gain
-    
+        """The gain applied to all inputs equally"
+        This example sets the input gain to 10dB.
+        .. code-block:: python
+        bd3491fs.input_gain = adafruit_bd3491fs.Level.10_DB""
+        """
+        return self._current_input_gain
+
     @input_gain.setter
     def input_gain(self, value):
         allowed_gains = [0, 1, 2, 3, 4, 6, 8, 10]
-        if not( value in allowed_gains):
+        if not value in allowed_gains:
             raise ValueError("input gain must be one of 0, 2, 4, 6, 8, 12, 16, 20 dB")
-        
+        self._input_gain = value
+        self._current_input_gain = value
 
     @property
-    def channel_1_gain(self):
-        "The gain applied to channel 1 of the currently selected input pair in -dB"
-        self._volume_gain_ch1
+    def channel_1_attenuation(self):
+        """The attenuation applied to channel 1 of the currently selected input pair in -dB.
+        Maximum is -87dB. To mute set to 255
+        This example sets the attenuation for input channel 1 to -10dB.
+        .. code-block:: python
+        bd3491fs.channel_1_attenuation = 10""
+        """
+        return self._current_ch1_attenuation
 
-    @channel_1_gain.setter
-    def channel_1_gain(self, value):
-        if ((value < 0 ) or (value > 87)):
-            raise ArgumentError("channel gain must be from 0-87db")
-        self._volume_gain_ch1 = value
-    
-    @property
-    def channel_2_gain(self):
-        "The gain applied to channel 2 of the currently selected input pair in -dB"
-        return self._volume_gain_ch2
-
-    @channel_2_gain.setter
-    def channel_2_gain(self, value):
-        if ((value < 0 ) or (value > 87)):
-            raise ArgumentError("channel gain must be from 0-87db")
-        self._volume_gain_ch2 = value
+    @channel_1_attenuation.setter
+    def channel_1_attenuation(self, value):
+        if ((value < 0) or ((value > 87) and (value != 255))):
+            raise ValueError("channel 1 attenuation must be from 0-87db")
+        self._ch1_attenuation = value
+        self._current_ch1_attenuation = value
 
     @property
-    def bass_gain(self):
-        """The amount of gain applied to the bass channel in dB"""
-        return self._bass_gain
+    def channel_2_attenuation(self):
+        """The attenuation applied to channel 2 of the currently selected input pair in -dB.
+        Maximum is -87dB. To mute set to 255
+        This example sets the attenuation for input channel 2 to -10dB.
+        .. code-block:: python
+        bd3491fs.channel_2_attenuation = 10""
+        """
+        return self._current_ch2_attenuation
 
-    @bass_gain.setter
-    def bass_gain(self, value):
-        allowed_gains = [0, 1, 2, 3, 4, 6, 7]
-        if not( value in allowed_gains):
-            raise ValueError("input gain must be one of 0, 2, 4, 6, 8, 12, 14 dB")
-        self._bass_gain = value
-    
-    @property
-    def treble_gain(self):
-        """The amount of gain applied to the treble channel in dB"""
-        return self._treble_gain 
-
-    @treble_gain.setter
-    def treble_gain(self, value):
-        allowed_gains = [0, 1, 2, 3, 4, 6, 7]
-        if not( value in allowed_gains):
-            raise ValueError("input gain must be one of 0, 2, 4, 6, 8, 12, 14 dB")
-        self._treble_gain = value
-
+    @channel_2_attenuation.setter
+    def channel_2_attenuation(self, value):
+        if ((value < 0) or ((value > 87) and (value != 255))):
+            raise ValueError("channel 2 attenuation must be from 0-87db")
+        self._ch2_attenuation = value
+        self._current_ch2_attenuation = value
